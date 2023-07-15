10fb4af427f00b380023a16011c686e5f05fea04
FelipeSantos1343 <felipeproa.fcos@gmail.com>
2023-07-15 14:36:06 -0300

    ajustes no modelo e primeiros testes de seed

--- src/main/java/com/listaJogos/projeto/models/Jogo.java
+++ src/main/java/com/listaJogos/projeto/models/Jogo.java
@@ -2,6 +2,7 @@ package com.listaJogos.projeto.models;
 
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,20 +16,24 @@ public class Jogo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String titulo;
+    private Double pontuacao;
     private Integer ano;
     private String genero;
     private String plataformas;
     private String imagem;
+    @Column(columnDefinition = "TEXT")
     private String descricaoCurta;
+    @Column(columnDefinition = "TEXT")
     private String descricaoLonga;
 
     public Jogo(){
     }
 
-    public Jogo(long id, String titulo, Integer ano, String genero, String plataformas, String imagem,
+    public Jogo(long id, String titulo, Double pontuacao,Integer ano, String genero, String plataformas, String imagem,
             String descricaoCurta, String descricaoLonga) {
         this.id = id;
         this.titulo = titulo;
+        this.pontuacao = pontuacao;
         this.ano = ano;
         this.genero = genero;
         this.plataformas = plataformas;
@@ -51,6 +56,14 @@ public class Jogo {
 
     public void setTitulo(String titulo) {
         this.titulo = titulo;
+    }
+    
+    public Double getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(Double pontuacao) {
+        this.pontuacao = pontuacao;
     }
 
     public Integer getAno() {
